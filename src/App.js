fd96a5f97051ba4b1b2f5267f7de755618ec3e62
Sean Beach <sean.beach1@yahoo.com>
2020-07-16 22:59:50 -0400

    components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import FavoriteMovies from './components/FavoriteMovies'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Profile from './components/Profile'
+import PitchList from './components/PitchList'
+import Forum from './components/Forum'
 import logo from './logo.svg';
 import './App.css';
 
